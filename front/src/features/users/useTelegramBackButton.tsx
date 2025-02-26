3047dfdf1d1e987d784022a143551c62284028fe
Shahzod <sibragimov999@gmail.com>
2025-02-27 01:30:13 +0500

    total

--- front/src/features/users/useTelegramBackButton.tsx
+++ front/src/features/users/useTelegramBackButton.tsx
@@ -1,33 +1,37 @@
-import { useEffect, useRef } from "react";
-import { useNavigate, useLocation } from "react-router";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import { useNavigate } from "react-router";
 import { useURLState } from "../../hooks/useURLState";
 
 const useTelegramBackButton = () => {
   const { allParams, setParam } = useURLState();
   const navigate = useNavigate();
-  const location = useLocation();
-  const prevParamsRef = useRef(allParams); // Отслеживаем предыдущее состояние
+  const { pathname } = useLocation();
+  //   const prevParamsRef = useRef(allParams); // Отслеживаем предыдущее состояние
 
   useEffect(() => {
     const backButton = window.Telegram.WebApp.BackButton;
     if (!backButton) return;
 
+    if (pathname === "/") {
+      backButton.hide();
+      return;
+    }
     backButton.show();
 
-    const handleBack = () => {
-      const keys = Object.keys(allParams).reverse();
+    const keys = Object.keys(allParams).reverse();
+    console.log(keys);
 
-      if (keys.length > 0) {
-        // Закрываем последнюю открытую вкладку
-        const lastKey = keys.find((key) => allParams[key] === "true");
-        if (lastKey) setParam(lastKey, false);
-      } else {
-        // Если вкладок нет, проверяем путь
-        if (location.pathname !== "/") {
-          navigate("/");
-        } else {
-          backButton.hide();
-        }
+    const handleBack = () => {
+      if (keys.length === 0) {
+        navigate("/");
+        return;
+      }
+
+      const lastKey = keys.find((key) => allParams[key] === "true");
+
+      if (lastKey) {
+        setParam(lastKey, false);
       }
     };
 
@@ -37,25 +41,21 @@ const useTelegramBackButton = () => {
       backButton.hide();
       backButton.offClick(handleBack);
     };
-  }, [allParams, navigate, location.pathname, setParam]);
+  }, [allParams, navigate, setParam]);
 
-  useEffect(() => {
-    const backButton = window.Telegram.WebApp.BackButton;
+  //   useEffect(() => {
+  //     const backButton = window.Telegram.WebApp.BackButton;
 
-    // Проверяем, было ли закрытие всех вкладок
-    if (
-      Object.keys(allParams).length === 0 &&
-      Object.keys(prevParamsRef.current).length > 0
-    ) {
-      if (location.pathname !== "/") {
-        navigate("/");
-      } else {
-        backButton.hide();
-      }
-    }
+  //     // Проверяем, был ли параметр удален и не добавился ли снова
+  //     if (
+  //       Object.keys(allParams).length === 0 &&
+  //       Object.keys(prevParamsRef.current).length > 0
+  //     ) {
+  //       backButton.hide();
+  //     }
 
-    prevParamsRef.current = allParams; // Обновляем предыдущее состояние
-  }, [allParams, navigate, location.pathname]);
+  //     prevParamsRef.current = allParams; // Обновляем предыдущее состояние
+  //   }, [allParams]);
 };
 
 export default useTelegramBackButton;
