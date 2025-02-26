a8657a09f4283d3895bd709245e1d51b91d621d8
Shahzod <sibragimov999@gmail.com>
2025-02-27 01:11:03 +0500

    total

--- front/src/features/users/useTelegramBackButton.tsx
+++ front/src/features/users/useTelegramBackButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router";
 import { useNavigate } from "react-router";
 import { useURLState } from "../../hooks/useURLState";
@@ -7,21 +7,21 @@ const useTelegramBackButton = () => {
   const { allParams, setParam } = useURLState();
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const prevParamsRef = useRef(allParams); // Отслеживаем предыдущее состояние
+  //   const prevParamsRef = useRef(allParams); // Отслеживаем предыдущее состояние
 
   useEffect(() => {
     const backButton = window.Telegram.WebApp.BackButton;
     if (!backButton) return;
 
+    if (pathname === "/") {
+      backButton.hide();
+      return;
+    }
     backButton.show();
 
     const handleBack = () => {
       const keys = Object.keys(allParams).reverse();
 
-      if (pathname === "/") {
-        backButton.hide();
-        return;
-      }
       if (keys.length === 0) {
         navigate(-1);
         return;
@@ -42,19 +42,19 @@ const useTelegramBackButton = () => {
     };
   }, [allParams, navigate, setParam]);
 
-  useEffect(() => {
-    const backButton = window.Telegram.WebApp.BackButton;
+  //   useEffect(() => {
+  //     const backButton = window.Telegram.WebApp.BackButton;
 
-    // Проверяем, был ли параметр удален и не добавился ли снова
-    if (
-      Object.keys(allParams).length === 0 &&
-      Object.keys(prevParamsRef.current).length > 0
-    ) {
-      backButton.hide();
-    }
+  //     // Проверяем, был ли параметр удален и не добавился ли снова
+  //     if (
+  //       Object.keys(allParams).length === 0 &&
+  //       Object.keys(prevParamsRef.current).length > 0
+  //     ) {
+  //       backButton.hide();
+  //     }
 
-    prevParamsRef.current = allParams; // Обновляем предыдущее состояние
-  }, [allParams]);
+  //     prevParamsRef.current = allParams; // Обновляем предыдущее состояние
+  //   }, [allParams]);
 };
 
 export default useTelegramBackButton;
